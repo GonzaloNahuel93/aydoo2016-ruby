cc09bae6984b19c76ac53a7250b7c9cf6c06d3a6
Gonzalo Nahuel <gonzalo.nahuel.dipierro@gmail.com>
2016-06-05 17:26:30 -0300

    FactoresPrimos Test --> Factoriza correctamente el numero 2

--- factores-primos/spec/FactoresPrimos_spec.rb
+++ factores-primos/spec/FactoresPrimos_spec.rb
@@ -3,6 +3,11 @@ require_relative '../model/FactoresPrimos'
 
 describe FactoresPrimos do
 
+  it 'Que tire [2] cuando se pide factorizar 2' do
+  	factoresPrimos = FactoresPrimos.new
+  	expect(factoresPrimos.factorizar(2)).to eq [2]
+  end
+
   it 'Que tire [2,5] cuando se pide factorizar 10' do
     factoresPrimos = FactoresPrimos.new
     expect(factoresPrimos.factorizar(10)).to eq [2,5]
